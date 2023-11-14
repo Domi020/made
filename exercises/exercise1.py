72ee105f5b00b8225e48b16043d717e3cd70cec6
Dominik Sauerer <dominik.sauerer@gmail.com>
2023-11-14 13:11:45 +0100

    Refactoring for ex 1

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -1,6 +1,16 @@
 import pandas as pd
 import sqlalchemy as sqla
 
+### CONSTANTS, URLS, FILE REFERENCES
+
+# address/file reference to the local sqlite db and table name
+sqlite_reference = 'sqlite:///airports.sqlite'
+table_name = 'airports'
+
+# url to the csv which has to be imported
+data_url = 'https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv'
+
+# column data type mapping
 type_dict = {
     "column_1": sqla.INTEGER,
     "column_2": sqla.TEXT,
@@ -17,16 +27,21 @@ type_dict = {
     "geo_punkt": sqla.TEXT,
 }
 
-# Create SQLAlchemy engine pointing to the sqlite file
-sqlite_file_engine = sqla.create_engine('sqlite:///airports.sqlite')
 
-csv_dataframe = pd.read_csv(filepath_or_buffer='https://opendata.rhein-kreis-neuss.de/api/v2/catalog/datasets/rhein-kreis-neuss-flughafen-weltweit/exports/csv',
-            sep=';', header=0)
 
-csv_dataframe.to_sql(name='airports', con=sqlite_file_engine, 
-                    if_exists='replace', 
+### MAIN PIPELINE
+
+# create SQLAlchemy engine pointing to the sqlite file
+sqlite_file_engine = sqla.create_engine(sqlite_reference)
+
+# download and read csv to dataframe (includes header => header=0)
+csv_dataframe = pd.read_csv(filepath_or_buffer=data_url, sep=';', header=0)
+
+# use dataframe method to directly import it to the sqlite engine
+csv_dataframe.to_sql(name=table_name, con=sqlite_file_engine, 
+                    if_exists='replace', # so that the script can be run multiple times without error/duplicate data
                     dtype=type_dict,
-                    index=False)
+                    index=False) # don't create extra index column
 
 
 
