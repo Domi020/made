73c5ad2a07b422b2b7addb4919da22c0828de5bf
Dominik Sauerer <dominik.sauerer@gmail.com>
2023-11-22 12:15:08 +0100

    Fix pipeline.sh

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+# IMPORTANT: Set these two variables for your own Destatis-Genesis account, if you want to run the Destatis pipeline
 GENESIS_USER=
 GENESIS_PASSWORD=
 
-source ../.venv/bin/activate
+cd "$(dirname "$0")"
 cd ..
 
 echo "Run pipeline 1/2: KBA"
 python project/kba_pipeline.py
+echo "pipeline 1 finished"
 
 echo "Run pipeline 2/2: Destatis Genesis"
-if [ -z ${GENESIS_USER+x} ];
-then echo "Please enter a Genesis-Destatis user and password (can be created for free) in this script to access their data collection";
-else python project/destatis_pipeline.py $GENESIS_USER $GENESIS_PASSWORD;
+if [ -z ${GENESIS_USER} ];
+then 
+    echo "Pipeline 2 not executed: Please enter a Genesis-Destatis user and password (can be created for free) in this script to access their data collection";
+    echo "pipeline 2 canceled";
+else 
+    python project/destatis_pipeline.py $GENESIS_USER $GENESIS_PASSWORD;
+    echo "pipeline 2 finished";
 fi
 
