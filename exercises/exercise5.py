2b08b07303b23197b825d1ccc5ba8a9f9887b05b
Dominik Sauerer <dominik.sauerer@gmail.com>
2024-01-30 09:41:35 +0100

    Improve documentation of ex 5

--- exercises/exercise5.py
+++ exercises/exercise5.py
@@ -24,30 +24,47 @@ type_dict = {
 
 ### FUNCTIONS
 
-def retrieve_data(data_url: str) -> str:
+def retrieve_data(data_url: str, local_file_name: str, file_path_in_zip: str) -> str:
     """Download zip file and extract given file
-    :return path to extract file
+    :param data_url: the url to the online resource
+    :param local_file_name: the local file name under which the online zip should be saved to
+    :param file_path_in_zip: the name of the to be extracted file in the zip
+    :return path to the extracted file
     """
-    urllib.request.urlretrieve(data_url, "GTFS.zip")
-    with zipfile.ZipFile("GTFS.zip", 'r') as zip_directory:
-        return zip_directory.extract("stops.txt")
+    urllib.request.urlretrieve(data_url, local_file_name)
+    with zipfile.ZipFile(local_file_name, 'r') as zip_directory:
+        return zip_directory.extract(file_path_in_zip)
     
 def transform_dataframe(dataframe: pd.DataFrame) -> pd.DataFrame:
-    """Transform given dataframe according to the requirements"""
-    df = dataframe[["stop_id", "stop_name", "stop_lat", "stop_lon", "zone_id"]] # only those columns are needed
-    df = df[df["zone_id"] == 2001] # only data in zone 2001
+    """Transform and filter the given dataframe according to the requirements. The following steps are done:
+    - throw away unnecessary columns
+    - choose only data from zone 2001
+    :param dataframe: the to be transformed dataframe
+    :return the transformed dataframe
+    """
+    df = dataframe[["stop_id", "stop_name", "stop_lat", "stop_lon", "zone_id"]]
+    df = df[df["zone_id"] == 2001]
     return df
 
-def filter_dataframe(dataframe: pd.DataFrame) -> pd.DataFrame:
-    """Filter given dataframe according to the requirements"""
-    df = dataframe[(dataframe["stop_lat"] >= -90) & (dataframe["stop_lat"] <= 90)] # -90 <= stop_lat, stop_lon <= 90
+def validate_dataframe(dataframe: pd.DataFrame) -> pd.DataFrame:
+    """Validate given dataframe according to the requirements. The following steps are done:
+    - Only rows are kept where stop_lat/stop_lon are between -90 and 90
+    :param dataframe: the to be validated dataframe
+    :return the validated dataframe
+    """
+    df = dataframe[(dataframe["stop_lat"] >= -90) & (dataframe["stop_lat"] <= 90)]
     df = df[(df["stop_lon"] >= -90) & (df["stop_lon"] <= 90)]
     return df
 
-def load_dataframe_to_db(dataframe: pd.DataFrame) -> None:
-    """Load dataframe to sqlite file"""
+def load_dataframe_to_db(dataframe: pd.DataFrame, type_dict: dict, sqlite_file: str, table_name: str) -> None:
+    """Load dataframe to a sqlite file/table
+    :param dataframe: the to be saved data
+    :param type_dict: a python dict of {column_name : column_type} to map the correct types to the columns
+    :param sqlite_file: the path to the sqlite file (is created if it does not exist)
+    :param table_name: the table name under which the data should be saved
+    """
     # create SQLAlchemy engine pointing to the sqlite file
-    sqlite_file_engine = sqla.create_engine(sqlite_reference)
+    sqlite_file_engine = sqla.create_engine(sqlite_file)
 
     # use dataframe method to directly import it to the sqlite engine
     dataframe.to_sql(name=table_name, con=sqlite_file_engine, 
@@ -58,15 +75,15 @@ def load_dataframe_to_db(dataframe: pd.DataFrame) -> None:
 
 ### MAIN PIPELINE
 
-# download and read csv to dataframe (includes header => header=0)
-file_path = retrieve_data(data_url)
+# download and unzip GTFS data
+file_path = retrieve_data(data_url, "GTFS.zip", "stops.txt")
 
 # read csv to pandas dataframe
 csv_dataframe = pd.read_csv(filepath_or_buffer=file_path, sep=',', quotechar='"', header=0)
 
-# transform and filter the dataframe as needed
+# transform and validate the dataframe as needed
 df = transform_dataframe(csv_dataframe)
-df = filter_dataframe(df)
+df = validate_dataframe(df)
 
 # load to sqlite file
-load_dataframe_to_db(df)
+load_dataframe_to_db(df, type_dict, sqlite_reference, table_name)
